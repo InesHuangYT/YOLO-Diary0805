48a722891a340f3a4f40967d9bf5e6b37ab39bd4
rrou <qq649492@gmail.com>
2018-08-13 13:27:33 +0800

    controller分離

--- yolotest/src/main/java/com/example/repository/UserRepository.java
+++ yolotest/src/main/java/com/example/repository/UserRepository.java
@@ -9,6 +9,9 @@ import com.example.entity.User;
 import java.util.List;
 import java.util.Optional;
 
+//【spring boot 系列】spring data jpa 全面解析（实践 + 源码分析）
+//https://segmentfault.com/a/1190000015047290
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 	Optional<User> findByEmail(String email);
