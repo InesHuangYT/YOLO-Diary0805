a0eb30ac9e513d1c9549fc50910c21de4f389afc
Ines <inse52086@gmail.com>
2018-09-20 17:31:37 +0800

    改路徑

--- yolotest/src/main/java/com/example/controller/UploadDiaryPhotoController.java
+++ yolotest/src/main/java/com/example/controller/UploadDiaryPhotoController.java
@@ -55,7 +55,7 @@ import com.example.service.PhotoStorageService;
 @RequestMapping("/api/photo")
 public class UploadDiaryPhotoController {
 
-	static String PhotoFILEPATH = "/Users/ines/Desktop/engine/photo/";
+	static String PhotoFILEPATH = "C:/engine/photo/";
 	// --> C:/engine/photo/ -->windows's path
 	// --> /Users/ines/Desktop/engine/photo/ -->ines's mac path
 	// --> C:/Users/Administrator/Desktop/Engine0818/photo/ -->rou's path
