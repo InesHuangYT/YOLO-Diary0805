05658dcf9bf3067d3cf088b073128ffb179e8638
Rrou03 <qq649492@gmail.com>
2018-09-26 13:24:17 +0800

    新增/album/albums回傳相簿封面照片uri

--- yolotest/src/main/java/com/example/payload/AlbumResponse.java
+++ yolotest/src/main/java/com/example/payload/AlbumResponse.java
@@ -9,10 +9,25 @@ public class AlbumResponse {
 	private Long id;
 	private String name;
 	private List<Diary> diaries;
+	private String photouri;
 //	private List<DiaryResponse> diaries;
 
 	public AlbumResponse() {
 	}
+	
+	
+
+	public String getPhotouri() {
+		return photouri;
+	}
+
+
+
+	public void setPhotouri(String photouri) {
+		this.photouri = photouri;
+	}
+
+
 
 	public AlbumResponse(String name) {
 		this.name = name;
