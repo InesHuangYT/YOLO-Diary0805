574b068ec589623d2182881195886c6a9bfb7cb9
Rrou03 <qq649492@gmail.com>
2018-10-19 18:36:43 +0800

    Success Message

--- yolotest/src/main/java/com/example/exception/SuccessMessage.java
+++ yolotest/src/main/java/com/example/exception/SuccessMessage.java
@@ -1,5 +1,20 @@
 package com.example.exception;
 
-public class SuccessMessage {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.OK)
+public class SuccessMessage extends RuntimeException {
+	
+	private String user;
+	private String friend;
+	
+	public SuccessMessage(String user, String friend) {
+		super(String.format("%s add %s to friend list", user, friend));
+		this.user = user;
+		this.friend = friend;
+	}
+	
+	
 
 }
