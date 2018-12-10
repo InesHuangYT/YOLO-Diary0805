ea65c1e6f6e379a300b2d2024aec484be6876c86
Rrou03 <qq649492@gmail.com>
2018-12-10 19:25:00 +0800

    測試hashmap(0變數)

--- yolotest/src/main/java/com/example/entity/RecUser.java
+++ yolotest/src/main/java/com/example/entity/RecUser.java
@@ -36,43 +36,7 @@ public class RecUser {
 		FrameFacePath = frameFacePath;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((FrameFacePath == null) ? 0 : FrameFacePath.hashCode());
-		result = prime * result + ((ImageSourcePath == null) ? 0 : ImageSourcePath.hashCode());
-		result = prime * result + ((PersonId == null) ? 0 : PersonId.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		RecUser other = (RecUser) obj;
-		if (FrameFacePath == null) {
-			if (other.FrameFacePath != null)
-				return false;
-		} else if (!FrameFacePath.equals(other.FrameFacePath))
-			return false;
-		if (ImageSourcePath == null) {
-			if (other.ImageSourcePath != null)
-				return false;
-		} else if (!ImageSourcePath.equals(other.ImageSourcePath))
-			return false;
-		if (PersonId == null) {
-			if (other.PersonId != null)
-				return false;
-		} else if (!PersonId.equals(other.PersonId))
-			return false;
-		return true;
-	}
-
+	
 	
 	
 
