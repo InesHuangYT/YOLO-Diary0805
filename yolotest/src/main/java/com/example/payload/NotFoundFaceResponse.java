ce2a67b41fffa0ec248a2995728dafbfc751c3d2
Rrou03 <qq649492@gmail.com>
2018-12-11 22:45:55 +0800

    新增NotFoundFaceResponse

--- yolotest/src/main/java/com/example/payload/NotFoundFaceResponse.java
+++ yolotest/src/main/java/com/example/payload/NotFoundFaceResponse.java
@@ -2,6 +2,25 @@ package com.example.payload;
 
 public class NotFoundFaceResponse {
 	
+	byte[] NotFoundFaceData;
+
+	public NotFoundFaceResponse(byte[] notFoundFaceData) {
+		super();
+		NotFoundFaceData = notFoundFaceData;
+	}
+
+	public byte[] getNotFoundFaceData() {
+		return NotFoundFaceData;
+	}
+
+	public void setNotFoundFaceData(byte[] notFoundFaceData) {
+		NotFoundFaceData = notFoundFaceData;
+	}
+	
+	
+	
+	
+	
 	
 	
 
