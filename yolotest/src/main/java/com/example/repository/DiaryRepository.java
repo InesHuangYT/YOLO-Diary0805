c7faf12a47093788a74ed22013a330e6ad47de40
Rrou03 <qq649492@gail.com>
2018-12-20 22:56:51 +0800

    衝突調整

--- yolotest/src/main/java/com/example/repository/DiaryRepository.java
+++ yolotest/src/main/java/com/example/repository/DiaryRepository.java
@@ -18,8 +18,6 @@ public interface DiaryRepository extends JpaRepository<Diary, String> {
 	Optional<Diary> findById(String diaryId);
 
 	Optional<Diary> findByPhotoId(String photoId);
-	
-	Optional<Diary> findByAlbumIdAndCreatedBy(String albumId, String username);
 
 	Optional<Diary> findByAlbumIdAndCreatedBy(String albumId, String username);
 
